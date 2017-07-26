d0d8087a5dba59c6aa9d487eb40e81c363373023
Emmanuel Hmar <eman.hmar@gmail.com>
2017-07-26 16:22:12 -0400

    Fixed buttons not showing. Added methods to main.

--- app/src/main/java/com/javahelps/calculator/MainActivity.java
+++ app/src/main/java/com/javahelps/calculator/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
 public class MainActivity extends ActionBarActivity {
     //    IDs of all the the numeric buttons
     private int[] numericButtons = {R.id.btnZero, R.id.btnOne, R.id.btnThree, R.id.btnTwo, R.id.btnFour,
@@ -31,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
         this.textScreen = (TextView) findViewById(R.id.textScreen);
 //        Find and set OnClickListetner to numeric buttons
         setNumericOnClickListener();
-//        Find and set Onclicklistener to operator, equal and decimal buttons
+//        Find and set OnClickListener to operator, equal and decimal buttons
         setOperatorOnClickListener();
     }
 
@@ -49,8 +52,89 @@ public class MainActivity extends ActionBarActivity {
                     stateError = false;
                 } else {
 //                    If not, there is already a valid expression so append it
-
+                    textScreen.append(button.getText());
                 }
+//                Set the flag
+                lastNumeric = true;
+            }
+        };
+//        Assign the listener to all the numeric buttons
+        for (int id : numericButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    //        Find and set OnClickListener to operator, equal and decimal buttons
+    private void setOperatorOnClickListener() {
+//        Create a common OnClickListener for operators
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                If current State is false do not append
+//                If the last input is number only, append the operator
+                if (lastNumeric && !stateError) {
+                    Button button = (Button) view;
+                    textScreen.append(button.getText());
+                    lastNumeric = false;
+                    lastDot = false;//Reset the DOT flag
+                }
+            }
+        };
+//        Assign the listener to all operator buttons
+        for (int id : operatorButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+//        Decimal point
+        findViewById(R.id.btnDot).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if (lastNumeric && !stateError && !lastDot) {
+                    textScreen.append(".");
+                    lastNumeric =false;
+                    lastDot = true;
+                }
+            }
+        });
+//        Clear button
+        findViewById(R.id.btnClear).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                textScreen.setText("");//Clear the screen
+//                REset all the states and flags
+                lastNumeric = false;
+                stateError = false;
+                lastDot =false;
+            }
+        });
+
+        //Equal Button
+        findViewById(R.id.btnEqual).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                onEqual();
+            }
+        });
+    }
+
+//    logic to calculate the solution
+    private void onEqual() {
+//        If the current state is error, do nothing
+//        If the last input is a number only, solution can be found
+        if (lastNumeric && !stateError) {
+//            Read the expression
+            String text = textScreen.getText().toString();
+//            Create an expression (A class from exp4j library)
+            Expression expression = new ExpressionBuilder(text).build();
+            try {
+//                Calculate the result and display
+                double result = expression.evaluate();
+                textScreen.setText(Double.toString(result));
+                lastDot = true; //Result contains a dot
+            } catch (ArithmeticException ex) {
+//                Display an error message
+                textScreen.setText("Error");
+                stateError = true;
+                lastNumeric = false;
             }
         }
     }
